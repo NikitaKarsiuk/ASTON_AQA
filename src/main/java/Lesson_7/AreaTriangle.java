b1d9f7474b36a3b7db04e641ffff1070d737778d
Nikita Karsiuk <125991680+NikitaKarsiuk@users.noreply.github.com>
2025-09-21 02:51:10 +0300

    updated testng.xml

--- src/main/java/Lesson_7/AreaTriangle.java
+++ src/main/java/Lesson_7/AreaTriangle.java
@@ -1,8 +1,7 @@
 package Lesson_7;
 
 public class AreaTriangle {
-    public static double getAreaTriangle(double sideA, double h)
-    {
+    public static double getAreaTriangle(double sideA, double h) {
         return 0.5 * sideA * h;
     }
 }
